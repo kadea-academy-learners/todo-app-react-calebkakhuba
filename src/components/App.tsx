f361cfbed21ba61ee35546decc7f7bfb130638ae
caleb kakhuba <caleb.k.mupalanga@gmail.com>
2024-08-30 12:22:23 +0100

    améliorations

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const [tasks, setTasks] = useState([]); // State pour stocker les taches
   const [newTask, setNewTask] = useState(''); // State pour une nouvelle tache
 
-  //condition 
+  //validation pour la saisie de la task
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
       setTasks([...tasks, { text: newTask, completed: false }]);
-      setNewTask(''); // Pour Réinitialiser le champ de saisie
+      setNewTask(''); // Pour réinitialiser le champ de saisie une fois la validation passée
     }
   };
 
@@ -18,11 +18,11 @@ function App() {
       <div>
           <h1>TODO LIST With <span>#React</span></h1>
           <p>
-          Pour la gestion ordonée des taches !!! <br /> <span>Nommez,</span> <br /> <span>Ajoutez,</span> <br /><span>Marquez</span> et <br /><span>Supprimez</span> vos taches librement...
+          Pour la gestion ordonée des taches !!! <br /> <span>Nommez,</span> <span>Ajoutez,</span> <span>Marquez</span> et <span>Supprimez</span> vos taches librement...
           </p>
       </div>
       <div>
-        <h2>Ma Liste de Tâches</h2>
+        <h2>Ma Liste des Tâches</h2>
         <input
           type="text"
           value={newTask}
